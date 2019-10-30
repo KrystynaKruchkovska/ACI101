67ec644b831330ce4aa611501da95abd97b61d63
Krystyna Kruchkovska <kruchok883@gmail.com>
2019-10-30 13:22:17 +0100

    AppCenter start

---
+++ Start/Minutes/AppCenter/AppCenter.h
@@ -0,0 +1,19 @@
+//
+//  AppCenter.h
+//  AppCenter
+//
+//  Created by Krystyna Kruchkovska on 10/30/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppCenter.
+FOUNDATION_EXPORT double AppCenterVersionNumber;
+
+//! Project version string for AppCenter.
+FOUNDATION_EXPORT const unsigned char AppCenterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCenter/PublicHeader.h>
+
+
--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,4 +1,9 @@
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+import AppCenterDistribute
+import AppCenterPush
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -8,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        MSAppCenter.start("d593639a-35a5-4355-9d16-a520fc003221" , withServices: [MSDistribute.self, MSAnalytics.self, MSPush.self, MSCrashes.self])
         return true
     }
 
